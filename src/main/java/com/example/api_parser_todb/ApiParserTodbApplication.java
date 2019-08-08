b9ff73feef689e091ebb2aae7b9fd496111a182f
Daniil Miroshnykov <danielq911q@gmail.com>
2019-08-08 15:39:40 +0300

    Initial commit.

---
+++ src/main/java/com/example/api_parser_todb/ApiParserTodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_parser_todb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiParserTodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiParserTodbApplication.class, args);
+    }
+
+}
